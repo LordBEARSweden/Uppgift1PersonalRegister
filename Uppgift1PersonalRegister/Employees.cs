3432545c81f7cb59ca136425935e8a4191971b01
Björn Andersson <lord_bear@hotmail.com>
2021-01-20 15:20:38 +0100

    Försöker fixa lista

---
+++ Uppgift1PersonalRegister/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift1PersonalRegister
+{
+    class Employees
+    {
+        public string firstName;
+        public string lastName;
+        public int salary;
+
+    }
+
+}
---
+++ Uppgift1PersonalRegister/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/* Fråga 1: De klasser som borde ingå i programmet är en personalclass och eventuellt en löneklass
+ * fråga 2: De attribut och metoder som borde ingå är input, för att fylla i personalens uppgifter,
+ * en consol så att man kan välja att registrera personer eller visa register.
+ */
+
+namespace Uppgift1PersonalRegister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            public void list()
+            {
+                Console.WriteLine(firstName + lastName + salary);
+            } 
+        }
+    }
+}
